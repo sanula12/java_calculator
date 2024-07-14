9213a5ba85c83ab1efc67c46a68f7211e37e6687
Sanula Kariyapperuma <sanula2021@gmail.com>
2024-07-14 23:25:42 +0530

    first-commit

--- src/gui/C.java
+++ src/gui/C.java
@@ -23,7 +23,7 @@ class Cal implements ActionListener {
     TextField tf;
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11,
             b12, b13, b14, b15, b17, b18,
-            b19, b20, b24;
+            b19, b20, b24,b25;
 
     String fv, sv, op;
     Double fdv,sdv,tot;
